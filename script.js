a97328dd43229d9534c642246ab4a4f3be048a59
Desilvavelasquez <163108056+desilvavelasquez123@users.noreply.github.com>
2024-09-29 16:38:27 +0200

    Revamp of functions - the game is played in rounds
    - All functions are nested within a main function
    - Only score variables are global
    - Multi-line variable declaration globally
    - Alert for the final score added
    - Draw score increment and printing addition

--- script.js
+++ script.js
@@ -1,55 +1,58 @@
 console.log("Hello World!");
 
+let humanScore = 0,
+computerScore = 0,
+draw = 0;
+
+
+function playGame() {
+
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
-let humanChoice = null;
 let computerChoice = null;
-let humanScore = 0;
-let computerScore = 0;
- 
-function getComputerChoice(computerChoice) {
-    computerChoice = Math.floor(Math.random() * 100);
-    if (computerChoice <= 33) {
-        computerChoice = rock;
-        return computerChoice;
-    } else if (computerChoice <= 66) {
-        computerChoice = paper;
-        return computerChoice;
-    } else {
-        computerChoice = scissors;}
-        return computerChoice;
-}
-getComputerChoice(computerChoice);
+let humanChoice = null;
 
-function getHumanChoice(humanChoice) {
-    humanChoice = prompt("Please type either rock, paper, or scissors").toLowerCase();
-    if (humanChoice === ("rock")) {
-        humanChoice = rock;
-        return humanChoice;
+    function getComputerChoice(choice) {
+        computerChoice = Math.floor(Math.random() * 100);
+        if (computerChoice <= 33.33) {
+            computerChoice = rock;
+            return computerChoice;
+        } else if (computerChoice <= 66.66) {
+            computerChoice = paper;
+            return computerChoice;
+        } else {
+            computerChoice = scissors;}
+            return computerChoice;
     }
-    else if (humanChoice === ("paper")) {
-        humanChoice = paper;
-        return humanChoice;
+
+    getComputerChoice(computerChoice);
+
+    function getHumanChoice(choice) {
+        humanChoice = (prompt("Please type either rock, paper, or scissors")).toLowerCase();
+        if (humanChoice === ("rock")) {
+            humanChoice = rock;
+            return humanChoice;
+        }
+        else if (humanChoice === ("paper")) {
+            humanChoice = paper;
+            return humanChoice;
+        }
+        else if (humanChoice === ("scissors")) {
+            humanChoice = scissors
+            return humanChoice;
+        } else {
+            alert("Choose one of the actual choices, don't goof around! :D")
+            return getHumanChoice(humanChoice);
+        }
     }
-    else if (humanChoice === ("scissors")) {
-        humanChoice = scissors
-        return humanChoice;
-    } else {
-        alert("Choose one of the actual choices, don't goof around! :D")
-        return getHumanChoice(humanChoice);
-    }
-}
 
-computerChoice = getComputerChoice(true);
-humanChoice = getHumanChoice(true);
+    getHumanChoice(humanChoice);
 
-console.log(humanChoice,computerChoice);
-
-function playRound(computerChoice,humanChoice) {
+function playRound(choiceComputer,choiceHuman) {
     if ((computerChoice === rock && humanChoice === rock) || (computerChoice === paper && humanChoice === paper) || (computerChoice === scissors && humanChoice === scissors)) {
     alert(`It's a draw! You both chose ${humanChoice}!`);
-    return;
+    return draw ++;
     } else if ((computerChoice === rock && humanChoice === scissors) || (computerChoice === paper && humanChoice === rock) || (computerChoice === scissors && humanChoice === paper)) {
         alert(`You lose! You chose ${humanChoice}, the computer got ${computerChoice}`);
         return computerScore ++;
@@ -59,6 +62,14 @@ function playRound(computerChoice,humanChoice) {
     }
 }
 
-playRound(computerChoice,humanChoice);
+playRound();
+console.log(humanScore,computerScore,draw);
+}
 
-console.log(humanScore,computerScore)
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
+
+alert(`Final score is: You: ${humanScore} Computer: ${computerScore} Draw: ${draw}`);
