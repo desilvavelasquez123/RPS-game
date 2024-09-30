62be1a20f192d7dac93f9f5a6977264f22b2de52
Desilvavelasquez <163108056+desilvavelasquez123@users.noreply.github.com>
2024-09-30 23:37:26 +0200

    Just add basic comments

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-alert("Hello, visitor! Welcome to my Rock-Paper-Scissors game page! You will play 5 rounds against your device. Please select 'OK' to close this windows and start the game! Good luck!");
+alert("Hello, visitor! Welcome to my Rock-Paper-Scissors game page! You will play 5 rounds against your device. Please select 'OK' to close this windows and start the game! Good luck!"); // greetings
+
+// global variables for scoring
 
 let humanScore = 0,
 computerScore = 0,
 draw = 0;
 
+// main function/game
+
 function playGame() {
+
+// choice variables
 
 const rock = "rock",
 paper = "paper",
 scissors = "scissors";
 
+// round function
+
     function playRound() {
+
+// computer chooses first
 
         let computerChoice = () => {
 
@@ -25,6 +35,8 @@ scissors = "scissors";
     
         let computerMove = computerChoice();
     
+// human choice via prompt()
+
         function humanChoice() {
             let choice = prompt("Please type either rock, paper, or scissors").trim().toLowerCase();
             if (choice === rock || choice === paper || choice === scissors) {
@@ -36,6 +48,8 @@ scissors = "scissors";
         }
     
         let humanMove = humanChoice();
+
+// scoring algorithm
 
     if ((computerMove === rock && humanMove === rock) || (computerMove === paper && humanMove === paper) || (computerMove === scissors && humanMove === scissors)) {
     alert(`It's a draw! You both chose ${humanMove}!`);
@@ -52,6 +66,9 @@ scissors = "scissors";
 playRound();
 console.log(humanScore,computerScore,draw);
 }
+
+// for-loop alternative
+
 
 playGame();
 playGame();
